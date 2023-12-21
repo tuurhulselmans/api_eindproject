dfba30476409fc94964d950e74c8cff1851bc2cd
Tuur Hulselmans <007tuur@gmail.com>
2023-12-21 19:35:12 +0100

    testfile

--- myproject/test_endpoints.py
+++ myproject/test_endpoints.py
@@ -1,51 +1,102 @@
 import requests
 import pytest
 
-# De basis-URL van je API
-BASE_URL = "http://localhost:8000"
+BASE_URL = "http://localhost:8000"  # Replace with your API URL
 
-# Test voor het GET-eindpunt '/forecast/'
-def test_get_forecasts():
-    response = requests.get(f"{BASE_URL}/forecast/")
-    assert response.status_code == 200  # Controleer of de statuscode 200 is (OK)
+# Test user data for creation and login
+test_user_data = {
+    "email": "test@example.be",
+    "password": "test_password"
+}
+
+# User authentication token
+auth_token = None
+
+# Forecast ID placeholder
+forecast_id = 1
+
+# User ID placeholder
+user_id = 1
+
+# Test for creating a user
+def test_create_user():
+    global auth_token, user_id
+    response = requests.post(f"{BASE_URL}/users/", json=test_user_data)
+    assert response.status_code == 200  # Assuming 200 is the success status code
+    user_id = response.json().get("id")
+    auth_token = response.json().get("access_token")
 
 
-# Test voor het GET-eindpunt '/forecast/{forecast_id}'
+
+# Test for user login
+def test_user_login():
+    global auth_token
+    response = requests.post(f"{BASE_URL}/token", data={
+        "username": test_user_data["email"],
+        "password": test_user_data["password"]
+    })
+    assert response.status_code == 200  # Assuming 200 is the success status code
+    auth_token = response.json().get("access_token")
+    assert auth_token is not None
+
+
+# Test for posting a forecast
+def test_post_forecast():
+    global auth_token, forecast_id
+    headers = {"Authorization": f"Bearer {auth_token}"}
+    forecast_data = {
+        "city": "YourCity",
+        "date": "2024-01-01",
+        "description": "Sunny day",
+        "temperature_high": 30,
+        "temperature_low": 20
+    }
+    response = requests.post(f"{BASE_URL}/forecast/", headers=headers, json=forecast_data)
+    assert response.status_code == 200  # Assuming 200 is the success status code
+    forecast_id = response.json().get("id")
+    assert forecast_id is not None
+
+
+# Test for getting forecast data by ID
 def test_get_forecast_by_id():
-    forecast_id = 1  # Voer de test uit met een bestaand forecast_id
-    response = requests.get(f"{BASE_URL}/forecast/{forecast_id}")
-    assert response.status_code == 200  # Controleer of de statuscode 200 is (OK)
+    global auth_token, forecast_id
+    headers = {"Authorization": f"Bearer {auth_token}"}
+    response = requests.get(f"{BASE_URL}/forecast/{forecast_id}", headers=headers)
+    assert response.status_code == 200  # Assuming 200 is the success status code
+    forecast_data = response.json()
+    assert forecast_data.get("id") == forecast_id
 
 
-# Test voor het GET-eindpunt '/forecast_ordered/{city}'
-def test_get_forecast_ordered_by_city():
-    city = "Turnhout"  # Voer de test uit met een bestaande stad
-    response = requests.get(f"{BASE_URL}/forecast_ordered/{city}")
-    assert response.status_code == 200  # Controleer of de statuscode 200 is (OK)
+# Test for getting forecast data
+def test_get_forecasts():
+    global auth_token
+    headers = {"Authorization": f"Bearer {auth_token}"}
+    response = requests.get(f"{BASE_URL}/forecast/", headers=headers)
+    assert response.status_code == 200  # Assuming 200 is the success status code
+    forecast_data = response.json()
+    assert len(forecast_data) > 0  # Check if the forecast data is received
 
 
-# Test voor het GET-eindpunt '/locations/{location_id}'
-def test_get_location_by_id():
-    location_id = 1  # Voer de test uit met een bestaand location_id
-    response = requests.get(f"{BASE_URL}/locations/{location_id}")
-    assert response.status_code == 200  # Controleer of de statuscode 200 is (OK)
-
-
-# Test voor het GET-eindpunt '/users/'
-def test_get_users():
-    response = requests.get(f"{BASE_URL}/users/")
-    assert response.status_code == 200  # Controleer of de statuscode 200 is (OK)
-
-
-# Test voor het GET-eindpunt '/users/{user_id}'
+# Test for getting user data by ID
 def test_get_user_by_id():
-    user_id = 1  # Voer de test uit met een bestaand user_id
-    response = requests.get(f"{BASE_URL}/users/{user_id}")
-    assert response.status_code == 200  # Controleer of de statuscode 200 is (OK)
+    global auth_token, user_id
+    headers = {"Authorization": f"Bearer {auth_token}"}
+    response = requests.get(f"{BASE_URL}/users/{user_id}", headers=headers)
+    assert response.status_code == 200  # Assuming 200 is the success status code
+    user_data = response.json()
+    assert user_data.get("id") == user_id
 
 
+# Test for getting user data
+def test_get_user():
+    global auth_token
+    headers = {"Authorization": f"Bearer {auth_token}"}
+    response = requests.get(f"{BASE_URL}/users/", headers=headers)
+    assert response.status_code == 200  # Assuming 200 is the success status code
+    user_data = response.json()
+    assert len(user_data) > 0  # Check if the user data is received
 
 
-# Test uitvoeren met pytest
+# Run tests
 if __name__ == "__main__":
     pytest.main()
